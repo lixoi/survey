35cdf64ace92fdb9fdbd4179ed3556f89cc5ea0f
Aleksey Lixoi <al_dashing@list.ru>
2024-02-06 22:12:38 +0300

    test

--- migrations/21062023_calendar_init.sql
+++ migrations/21062023_calendar_init.sql
@@ -18,7 +18,7 @@ CREATE table survey (
     question_number number
 );
 
-CREATE users (
+CREATE table users (
     id                        serial primary key,
     base_questions            question_table,      
     first_profile_questions   question_table,
@@ -28,47 +28,84 @@ CREATE users (
     exist_to                  timestamptz not null default now()+interval '3 day'
 ); 
 
-CREATE linux_questions (
+CREATE table linux_questions (
     id          serial primary key,
     question    text,
     description text
 );
 
-CREATE k8s_guestions (
+CREATE table k8s_guestions (
     id          serial primary key,
     question    text,
     description text
 );
 
-CREATE network_questions (
+CREATE table network_questions (
     id          serial primary key,
     question    text,
     description text
 );
 
-CREATE information_security_questions (
+CREATE table information_security_questions (
     id          serial primary key,
     question    text,
     description text
 );
 
-CREATE container_questions (
+CREATE table container_questions (
     id          serial primary key,
     question    text,
     description text
 );
 
-CREATE developer_guestions (
+CREATE table developer_guestions (
     id          serial primary key,
     question    text,
     description text
 );
 
-INSERT INTO events (id, title, created_at, description)
+INSERT INTO linux_questions (title, description)
 VALUES
-    (1, 'test 1', now(), 'test description 1'),
-    (2, 'test 2', now(), 'test description 2'),
-    (3, 'test 3', now(), 'test description 3');
+    ('linux question 1', 'test linux description 1'),
+    ('linux question 2', 'test linux description 2'),
+    ('linux question 3', 'test linux description 3');
+
+INSERT INTO k8s_questions (title, description)
+VALUES
+    ('k8s question 1', 'test k8s description 1'),
+    ('k8s question 2', 'test k8s description 2'),
+    ('k8s question 3', 'test k8s description 3');
+
+INSERT INTO network_questions (title, description)
+VALUES
+    ('network question 1', 'network test description 1'),
+    ('network question 2', 'network test description 2'),
+    ('network question 3', 'network test description 3');
+
+INSERT INTO information_security_questions (title, description)
+VALUES
+    ('information security question 1', 'information security test description 1'),
+    ('information security question 2', 'information security test description 2'),
+    ('information security question 3', 'information security test description 3');
+
+INSERT INTO container_questions (title, description)
+VALUES
+    ('container question 1', 'container test description 1'),
+    ('container question 2', 'container test description 2'),
+    ('container question 3', 'container test description 3');
+
+INSERT INTO developer_guestions (title, description)
+VALUES
+    ('developer question 1', 'developer test description 1'),
+    ('developer question 2', 'developer test description 2'),
+    ('developer question 3', 'developer test description 3');
+
+INSERT INTO users (title, description)
+VALUES
+    ('developer question 1', 'developer test description 1'),
+    ('developer question 2', 'developer test description 2'),
+    ('developer question 3', 'developer test description 3');
+
 
 -- +goose Down
-drop table surveys;
+drop table survey;
